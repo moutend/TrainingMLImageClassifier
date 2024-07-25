211d5964e263e636de531861de000700ee4f838c
Yoshiyuki Koyanagi <moutend@gmail.com>
2024-07-25 16:34:06 +0900

    Create project

---
+++ TrainingMLImageClassifier/TrainingMLImageClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingMLImageClassifierApp.swift
+//  TrainingMLImageClassifier
+//
+//  Created by Yoshiyuki Koyanagi on 2024/07/25.
+//
+
+import SwiftUI
+
+@main
+struct TrainingMLImageClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
